fe55c5f50aa27228766d7c31fa86890b35400791
MabiGermano <mabi.germano.e@gmail.com>
2021-01-16 18:02:01 -0300

    reconstruindo

--- backend/src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const routes = Router();
-routes.get('/', (req:Request, res:Response) => { 
-    res.json({'title': 'Hello world!'}) 
-});
-
-export default routes;
