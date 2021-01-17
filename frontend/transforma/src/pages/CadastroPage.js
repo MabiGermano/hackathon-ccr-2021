75f990879b2cf699e4ba5bc99a0507444e77a3f4
luaoliver <lulu-so@hotmail.com>
2021-01-17 10:52:14 -0300

    Implementando botões

--- frontend/transforma/src/pages/CadastroPage.js
+++ frontend/transforma/src/pages/CadastroPage.js
@@ -13,7 +13,7 @@ const Cadastro = () => {
                         label="Nome Completo"
                     />    
                 </div>
-                <div div className="col s6">
+                <div className="col s6">
                     <Input
                         name="bornDate"
                         label="Data de Nascimento"
@@ -21,13 +21,13 @@ const Cadastro = () => {
                 </div>
             </div>
             <div className="row">
-                <div div className="col s6">
+                <div className="col s6">
                     <Input //Select?
                         name="genderIdentify"
                         label="Gênero"
                     />
                 </div>
-                <div div className="col s6">
+                <div className="col s6">
                     <Input
                         name="email"
                         label="Email"
@@ -35,13 +35,13 @@ const Cadastro = () => {
                 </div>
             </div>
             <div className="row">
-                <div div className="col s6">
+                <div className="col s6">
                     <Input //Select?
                         name="hasExperience"
                         label="Possui experiência?"
                     />
                 </div>
-                <div div className="col s6">
+                <div className="col s6">
                     <Input //Select?
                         name="occupationArea"
                         label="Área de interesse"
@@ -49,17 +49,29 @@ const Cadastro = () => {
                 </div>
             </div>
             <div className="row">
-                <div div className="col s6">
+                <div className="col s6">
                     <Input
                         name="experienceDescription"
                         label="Breve descrição sobre a experiência"
                     />
                 </div>
-                <div div className="col s6">
+                <div className="col s6">
                     <Input
                         name="mentorExpectations"
                         label="O que você espera de um mentor?"
                     />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s6">
+                    <button class="btn waves-effect black-text indigo lighten-4" type="submit" name="action">Cancelar
+                        <i class="material-icons right"></i>
+                    </button>
+                </div>
+                <div className="col s6">
+                    <button class="btn waves-effect black-text indigo lighten-4" type="submit" name="action">Enviar
+                        <i class="material-icons right"></i>
+                    </button>
                 </div>
             </div>
         </form>
