b1185ea316daa2e5edb39a153cb968be64ae7c7f
MabiGermano <mabi.germano.e@gmail.com>
2021-01-17 10:57:49 -0300

    init no login social com linkedin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,48 @@
 import express from 'express';
 import cors from 'cors';
+// import session from 'express-session';
+// import  passport from 'passport';
+// import { Strategy } from 'passport-linkedin-oauth2';
+import * as dotenv from 'dotenv';
+
 import routes from './routes';
 import './database/connection';
 
+// class LinkedInStrategy implements Strategy {
+
+// }
+
+dotenv.config();
+
 const app = express();
+// app.set('view engine', 'ejs');
+
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: 'SECRET'
+//   }));
+  
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+  
+//   passport.serializeUser(function (user, cb) {
+//     cb(null, user);
+//   });
+  
+//   passport.deserializeUser(function (obj, cb) {
+//     cb(null, obj);
+//   });
+  
+//   passport.use(new Strategy({
+//     clientID: process.env.LINKEDIN_CLIENT_ID,
+//     clientSecret: process.env.LINKEDIN_SECRET_ID,
+//     callbackURL: `${process.env.APP_BASE_URL}/home`,
+//     scope: ['r_emailaddress', 'r_liteprofile'],
+//   }, function (token, tokenSecret, profile, done) {
+//     return done(null, profile);
+//   }
+//   ));
 
 app.use(cors());
 app.use(express.json());
