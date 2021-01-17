8b3dd6215fbabe86b50cc9595831221bea781d58
MabiGermano <mabi.germano.e@gmail.com>
2021-01-17 01:33:09 -0300

    endpoint de salvamento no banco de dados

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,43 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import PersonStudent from './models/PersonStudent';
 
 const routes = Router();
 
-routes.get('/personStudent', (request, response) => {
+routes.get('/personStudent', (request: Request, response: Response) => {
     response.json({ list: [] });
 });
 
-routes.post('/personStudent', (req, resp) => {
-    
+routes.post('/personStudent', (request: Request, response: Response) => {
+    const {
+        name,
+        bornDate,
+        genderIdentity,
+        email,
+        hasExperience,
+        occupationArea,
+        experienceDescription,
+        mentorExpectations,
+    } = request.body;
+
+    const personStudentRepository = getRepository(PersonStudent);
+
+    const data = {
+        name,
+        bornDate,
+        genderIdentity,
+        email,
+        hasExperience,
+        occupationArea,
+        experienceDescription,
+        mentorExpectations,
+    };
+
+    const personStudent = personStudentRepository.create(data);
+
+    personStudentRepository.save(personStudent);
+    return response.status(201).json(personStudent);
+
 });
 
 export default routes;
