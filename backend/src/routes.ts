183785cfecf7264802779a24fea23176149d7cff
MabiGermano <mabi.germano.e@gmail.com>
2021-01-17 22:40:06 -0300

    Enviando email

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import PersonStudent from './models/PersonStudent';
+import nodemailer from 'nodemailer';
+import {config} from 'dotenv';
 
 const routes = Router();
 
@@ -42,5 +44,43 @@ routes.post('/personStudent', async (request: Request, response: Response) => {
     return response.status(201).json(personStudent);
 
 });
+
+routes.post('/conectar/:id', async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const personStudentRepository = getRepository(PersonStudent);
+    const person = await personStudentRepository.findOneOrFail(id);
+
+
+    var sender = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        service: 'gmail',
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_TO_SEND,
+            pass: process.env.PASSWORD_EMAIL
+        }
+    });
+
+    var emailToSend = {
+        from: 'mabi.germano.e@gmail.com',
+        to: person.email.toString(),
+        subject: '[MinasTalk] EBA! Um Mentor(a) Tem Interesse!!',
+        text: `Oi ${person.name}, um mentor(a) sinalizou interesse no seu perfil, e quer entrar em contato com você para começar imediatamente os estudos, não é legal?! 
+        Por favor, responda esse email com "Tenho interesse" e nossa equipe cuidará do primeiro contato entre vocês.
+        Caso o email não seja respondido em até 24h, entenderemos que você não tem interesse.
+        Abraços, equipe MinasTalk`,
+    };
+
+
+    sender.sendMail(emailToSend, function (error) {
+        if (error) {
+            return response.status(400).json({status: "error"});
+        } else {
+            return response.status(200).json({status: "success"});
+        }
+    });
+
+
+})
 
 export default routes;
