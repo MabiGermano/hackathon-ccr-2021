88cfc58ec968ca11e4b5f833a5143d9596b98294
luaoliver <lulu-so@hotmail.com>
2021-01-17 21:17:25 -0300

    Alterações landingpage

--- frontend/transforma/src/pages/LandingPage.js
+++ frontend/transforma/src/pages/LandingPage.js
@@ -5,6 +5,16 @@ import '../assets/css/landingpage.css';
 import presentationImg from '../assets/img/presentation-img1.png';
 import { Link } from 'react-router-dom';
 
+import MentorImg from '../assets/img/mentor-img.png';
+import Mentor1 from "../assets/img/mentor1.png";
+import Mentor2 from "../assets/img/mentor2.png";
+import Mentor3 from "../assets/img/mentor3.png";
+import Mentor4 from "../assets/img/mentor4.png";
+import Mentor5 from "../assets/img/mentor5.png";
+import Mentor6 from "../assets/img/mentor6.png";
+import Mentor7 from "../assets/img/mentor7.png";
+import Mentor8 from "../assets/img/mentor8.png";
+
 const Landing = () => {
     return (
         <div id="landing-page">
@@ -42,7 +52,7 @@ const Landing = () => {
                         </h5>
                     <h4>
                         Construir pontes entre jovens mulheres e profissionais de TI.
-                        </h4>
+                    </h4>
                 </div>
                 <div>
                     <p>
@@ -56,6 +66,31 @@ const Landing = () => {
                     <Link className="white pink-text text-darken-1 right s-mt-2 nav-button waves-effect waves-light btn">Inscreva-se grátis</Link>
                 </div>
             </footer>
+            <main>
+                <div className="row">
+                    <div id="Mentor-img" className="col m6">
+                        <img src={MentorImg} alt="Duas mulheres" />
+                    </div>
+
+                    <div id="Mentor" className="col m6">
+                        <div>
+                            <p className="tiny-text bold grey-text">
+                                — PROGRAMA DE MENTORIA
+                            </p>
+                            <h3>
+                                Quero ser um mentor
+                            </h3>
+                            <p className="tiny-text grey-text">
+                                Uma vez que você se inscrever, poderá agendar diretamente
+                                com a mentorada de acordo com a disponibilidade entre vocês.
+                            </p>
+                            <div className="action-buttons">
+                                <Link class="pink darken-1 waves-effect waves-light btn-large">Quero me inscrever</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main> 
         </div>
     );
 }
